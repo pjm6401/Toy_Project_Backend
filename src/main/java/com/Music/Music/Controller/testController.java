f60b56949466d996f5c8d3d9a2538ec4389f08b7
pjm6401 <pjm6401@naver.com>
2023-02-21 22:24:14 +0900

    .git ignore plus

---
+++ src/main/java/com/Music/Music/Controller/testController.java
@@ -0,0 +1,20 @@
+package com.Music.Music.Controller;
+
+import com.Music.Music.DTO.TestDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    TestDTO testDTO;
+    @GetMapping("/Music/hello")
+    public TestDTO test(){
+        System.out.println("Build react");
+        testDTO = new TestDTO();
+        testDTO.setId("1234@naver.com");
+        testDTO.setPw("1234");
+        testDTO.setName("Min_98");
+        testDTO.setAge("24");
+        return testDTO;
+    }
+}
---
+++ src/main/java/com/Music/Music/DTO/TestDTO.java
@@ -0,0 +1,22 @@
+package com.Music.Music.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class TestDTO {
+
+    String id;
+    String pw;
+    String name;
+    String age;
+
+
+    public TestDTO() {
+
+    }
+}
